59a9239b375985a3bb6620d53b443082107779fd
nooneknows peter <jerelepeter06@gmail.com>
2024-12-27 21:59:44 +0000

    Save progress: 1.1

--- salon.sh
+++ salon.sh
@@ -6,8 +6,10 @@ echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
 # global vars
 SERVICE_ID_SELECTED="service_id"
+NAME_OF_SERVICE="name of service"
 CUSTOMER_PHONE="customer's phone number"
 CUSTOMER_NAME="customer's name"
+CUSTOMER_ID="id of customer"
 SERVICE_TIME="time customer wants their service"
 
 # main menu
@@ -31,7 +33,12 @@ MAIN_MENU() {
   # user input for selected service
   read SERVICE_ID_SELECTED
 
-  VALID_SERVICE=$($PSQL "SELECT service_id, name FROM services where service_id=$SERVICE_ID_SELECTED")
+  VALID_SERVICE=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  NAME_OF_SERVICE=$($PSQL "SELECT name FROM services WHERE service_id = $VALID_SERVICE")
+
+  # # formats service name by removing spaces
+  # NAME_OF_SERVICE=$(echo $SERVCE | sed 's/ |/"/')
 
   if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]; then
     # send to main menu
@@ -40,13 +47,13 @@ MAIN_MENU() {
     MAIN_MENU "I could not find that service. Try again."
   else
     # calls function to query and insert user information
-    BOOKING
+    USER_INFORMATION
   fi
 
 }
 
 # query and input user information
-BOOKING() {
+USER_INFORMATION() {
 
   # conditional to print first argument
   if [[ $1 ]]; then
@@ -59,9 +66,9 @@ BOOKING() {
   read CUSTOMER_PHONE
 
   # query phone number from database
-  QUERY_PHONE_NUMBER=$($PSQL "SELECT phone, name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
 
-  if [[ -z $QUERY_PHONE_NUMBER ]]; then
+  if [[ -z $CUSTOMER_NAME ]]; then
     # if query is empty, no matching phone number
     echo -e "\nI don't have a record for that phone number, what's your name?"
 
@@ -70,14 +77,28 @@ BOOKING() {
 
     INSERT_USER_INFORMATION=$($PSQL "INSERT INTO customers (phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
 
-    echo "What time would you like your cut, $CUSTOMER_NAME"
-
-    read SERVICE_TIME
+    BOOK_TIME
 
   else
-    echo "What time would you like your _, $CUSTOMER_NAME"
+
+    BOOK_TIME
   fi
 
+}
+
+# create appointment
+BOOK_TIME() {
+
+  echo -e "\nWhat time would you like your $(echo $NAME_OF_SERVICE | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+
+  read SERVICE_TIME
+
+  # query customer id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  echo -e "\nI have put you down for a $(echo $NAME_OF_SERVICE | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
 }
 
 MAIN_MENU
